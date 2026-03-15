9b9a021e668828d5cae4e3e3f59ae34d11dac444
Pesimistixxx <musienkoiy@gmail.com>
2026-03-15 21:44:43 +0300

    add say_bye function

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ from utils import weather_wttr, get_cbr_rates
 def say_hi():
     print('hello world')
 
+def say_bye():
+    print('goodbye')
+
 def main():
     city = 'Мытищи'
     say_hi()
     weather_wttr(city)
     get_cbr_rates()
+    say_bye()
 
 if __name__ == '__main__':
     main()
